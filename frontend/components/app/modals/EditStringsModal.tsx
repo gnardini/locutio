36643586d6aaef1f60e4029aea6969861763bea1
Gonza Nardini <nardinigonzalom@gmail.com>
2025-01-13 00:14:34 -0300

    Allow filtering by emtpy

--- frontend/components/app/modals/EditStringsModal.tsx
+++ frontend/components/app/modals/EditStringsModal.tsx
@@ -1,10 +1,10 @@
-
 import { Button, ButtonType } from '@frontend/components/common/Button';
 import { Loader } from '@frontend/components/common/Loader';
 import { Modal } from '@frontend/components/common/Modal';
 import { useFetchStringsQuery } from '@frontend/queries/strings/useFetchStringsQuery';
 import { useTranslateQuery } from '@frontend/queries/strings/useTranslateQuery';
-import React, { useEffect, useState, useCallback } from 'react';
+import { CheckBoxIcon } from '@frontend/svgs/CheckBoxIcon';
+import React, { useCallback, useEffect, useState } from 'react';
 import FileStringTranslationRow from '../views/FileStringTranslationRow';
 
 interface EditStringsModalProps {
@@ -30,13 +30,16 @@ const EditStringsModal: React.FC<EditStringsModalProps> = ({
   const { execute: translate, loading: translating, error: translateError } = useTranslateQuery();
   const [baseStrings, setBaseStrings] = useState<Record<string, string>>({});
   const [strings, setStrings] = useState<Record<string, string>>({});
+  const [onlyMissing, setOnlyMissing] = useState(false);
 
   const loadStrings = () => {
     fetchStrings({ organizationId, language, file }).then((result) => {
       if (result) {
         setBaseStrings(result.baseStrings);
         setStrings(result.strings);
-        const translatedCount = Object.values(result.strings).filter(value => value.length > 0).length;
+        const translatedCount = Object.values(result.strings).filter(
+          (value) => value.length > 0,
+        ).length;
         const totalCount = Object.keys(result.baseStrings).length;
         onStringsUpdated(file, translatedCount, totalCount);
       }
@@ -60,18 +63,21 @@ const EditStringsModal: React.FC<EditStringsModalProps> = ({
     }
   };
 
-  const handleUpdateString = useCallback((key: string, value: string) => {
-    setStrings((prevStrings) => {
-      const newStrings = {
-        ...prevStrings,
-        [key]: value,
-      };
-      const translatedCount = Object.values(newStrings).filter(str => str.length > 0).length;
-      const totalCount = Object.keys(baseStrings).length;
-      onStringsUpdated(file, translatedCount, totalCount);
-      return newStrings;
-    });
-  }, [baseStrings, file, onStringsUpdated]);
+  const handleUpdateString = useCallback(
+    (key: string, value: string) => {
+      setStrings((prevStrings) => {
+        const newStrings = {
+          ...prevStrings,
+          [key]: value,
+        };
+        const translatedCount = Object.values(newStrings).filter((str) => str.length > 0).length;
+        const totalCount = Object.keys(baseStrings).length;
+        onStringsUpdated(file, translatedCount, totalCount);
+        return newStrings;
+      });
+    },
+    [baseStrings, file, onStringsUpdated],
+  );
 
   if (error || translateError)
     return (
@@ -87,7 +93,7 @@ const EditStringsModal: React.FC<EditStringsModalProps> = ({
       className="max-w-[90%] md:max-w-[1000px] max-h-[90%] p-3 md:p-8"
     >
       <h2 className="text-xl font-semibold mb-4">Edit Strings: {file}</h2>
-      <div className="mb-4">
+      <div className="mb-4 flex flex-row items-center justify-between">
         <Button
           type={ButtonType.Primary}
           onClick={handleTranslate}
@@ -96,6 +102,10 @@ const EditStringsModal: React.FC<EditStringsModalProps> = ({
         >
           {translating ? 'Translating...' : 'Translate'}
         </Button>
+        <div className="flex flex-row gap-2 items-center">
+          <p>Only Missing</p>
+          <CheckBoxIcon isSelected={onlyMissing} onClick={() => setOnlyMissing(!onlyMissing)} />
+        </div>
       </div>
       {(loading || translating) && <Loader className="mx-auto" />}
       {Object.entries(baseStrings).length > 0 && (
@@ -108,19 +118,21 @@ const EditStringsModal: React.FC<EditStringsModalProps> = ({
               </tr>
             </thead>
             <tbody>
-              {Object.entries(baseStrings).map(([key, baseValue]) => (
-                <FileStringTranslationRow
-                  key={key}
-                  organizationId={organizationId}
-                  language={language}
-                  file={file}
-                  baseLanguage={baseLanguage}
-                  stringKey={key}
-                  baseValue={baseValue}
-                  initialValue={strings[key] || ''}
-                  onUpdateString={handleUpdateString}
-                />
-              ))}
+              {Object.entries(baseStrings)
+                .filter(([key]) => !onlyMissing || !strings[key])
+                .map(([key, baseValue]) => (
+                  <FileStringTranslationRow
+                    key={key}
+                    organizationId={organizationId}
+                    language={language}
+                    file={file}
+                    baseLanguage={baseLanguage}
+                    stringKey={key}
+                    baseValue={baseValue}
+                    initialValue={strings[key] || ''}
+                    onUpdateString={handleUpdateString}
+                  />
+                ))}
             </tbody>
           </table>
         </div>
